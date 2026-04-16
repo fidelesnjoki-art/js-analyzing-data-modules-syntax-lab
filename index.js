30a93a401e3e691cf920ea01ffc3aaf871446063
fidelis <fideles.njoki@student.moringaschool.com>
2026-04-16 08:45:25 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+// require('datejs')
+// function combineUsers(...args){
 
+//   const combineObject = {
+//     users:[]
+// };
+// args.forEach(subArr =>{
+//   combinedObject.users.push(...subArr);
+// });
+
+// combinedObject.mergeDate = today.toString("M/d/yyyy");
+// return combinedObjects;
+// }
+
+const datejs = require('datejs');
+
+function combineUsers (...args){
+  let combinedObject = {
+    users: []
+  };
+  args.forEach(subArr =>{
+    combinedObject.users.push(...subArr)
+  });
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+  
+  return combinedObject
+}
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+// module.exports = {
+//   ...(typeof combineUsers !== 'undefined' && { combineUsers })
+// };
